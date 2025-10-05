c58366ea92ec3e132d817773539ebf80c96aaad8
ApoorvaBajpai <apoorvabajpai2424@gmail.com>
2025-10-06 00:38:42 +0530

    changes in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,11 +64,15 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation("androidx.activity:activity-compose:1.11.0")
+
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -82,5 +86,8 @@ dependencies {
     implementation("com.google.ai.client.generativeai:generativeai:0.1.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.reactivestreams:reactive-streams:1.0.4")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
 
 }
